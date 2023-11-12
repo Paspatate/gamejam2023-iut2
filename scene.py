76211d57068cb3ee915dadb8509bac9a7a9318d9
tint0uin <titouan.descure@skeuden.net>
2023-11-12 20:20:30 +0100

    fix last input

--- scene.py
+++ scene.py
@@ -172,7 +172,7 @@ class Scene:
                     self.rep[self.numExo].append(self.listF[self.numExo][self.bulleManager.current  - sum_to(self.exo, self.numExo)])
                     Scene.sErreur.play()
                     
-                elif (not self.bulleManager.bulles[self.bulleManager.current-1].has_responded and not self.bulleManager.bulles[self.bulleManager.current-1].can_interact):
+                elif (not self.bulleManager.bulles[self.bulleManager.current-1].has_responded and not self.bulleManager.bulles[self.bulleManager.current-1].can_interact) and self.bulleManager.current != 0:
                     self.bulleManager.bulles[self.bulleManager.current-1].has_responded = True
                     if self.bulleManager.current-1 >= sum_to(self.exo, self.numExo):
                         self.rep[self.numExo].append(self.listF[self.numExo][self.bulleManager.current - sum_to(self.exo, self.numExo)-1])
